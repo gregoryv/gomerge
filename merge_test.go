6975e1014b6f4924480183b4805410e206bdbf4b
Gregory Vincic <g@7de.se>
2022-02-19 12:00:30 +0100

    Use gomerge on this repo

--- merge_test.go
+++ merge_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/gregoryv/golden"
@@ -32,4 +34,109 @@ func x() {}
 	}
 
 	golden.Assert(t, buf.String())
+}
+
+func ExampleSplit_header() {
+	data := []byte(`// my docs
+package x
+`)
+	s := Split(data)
+	fmt.Println(s.Header.String())
+	// output:
+	// // my docs
+}
+
+func ExampleSplit_single() {
+	data := []byte(`package x
+
+import "fmt"
+
+func x() {}
+`)
+	s := Split(data)
+	fmt.Println(s.Imports.String())
+	// output:
+	// import "fmt"
+}
+
+func ExampleSplit() {
+	data := []byte(`package x
+
+import (
+	"fmt"
+)
+
+func x() {}
+`)
+	s := Split(data)
+	fmt.Println(s.Imports.String())
+	// output:
+	//	import (
+	//	"fmt"
+	//)
+}
+
+func TestSplit(t *testing.T) {
+	t.Run("plain", func(t *testing.T) {
+		data := []byte(`package x`)
+		s := Split(data)
+		if s.Header.Len() != 0 {
+			t.Error("found header: ", s.Header.String())
+		}
+		if s.Package.Len() == 0 {
+			t.Error("empty package")
+		}
+		if s.Imports.Len() != 0 {
+			t.Error("found imports: ", s.Imports.String())
+		}
+		if s.Rest.Len() != 0 {
+			t.Error("found rest: ", s.Rest.String())
+		}
+	})
+
+	t.Run("one import", func(t *testing.T) {
+		data := []byte(`package x
+
+import "fmt"
+`)
+		s := Split(data)
+		if s.Header.Len() != 0 {
+			t.Error("found header:", s.Header.String())
+		}
+		if strings.Contains(s.Header.String(), "import") {
+			t.Error("found import in header")
+		}
+		if s.Imports.Len() == 0 {
+			t.Error("empty imports")
+		}
+		if s.Rest.Len() != 0 {
+			t.Error("found rest: ", s.Rest.String())
+		}
+	})
+
+	t.Run("one import and body", func(t *testing.T) {
+		data := []byte(`// my docs
+package x
+
+import (
+	"fmt"
+	"strings"
+)
+
+func x() {}
+`)
+		s := Split(data)
+		if s.Header.Len() == 0 {
+			t.Error("empty header")
+		}
+		if strings.Contains(s.Header.String(), "import") {
+			t.Error("found import in header")
+		}
+		if s.Imports.Len() == 0 {
+			t.Error("empty imports")
+		}
+		if s.Rest.Len() == 0 {
+			t.Error("empty rest")
+		}
+	})
 }
